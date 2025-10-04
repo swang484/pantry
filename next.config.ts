ce9c3fb12a3ad78f5bff54a8738a5569ecb74a6f
Annabel Qin <annabel.qin@nue.io>
2025-10-04 14:19:11 -0400

    pantry

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+    unoptimized: true, // This will help with local development
+  },
 };
 
 export default nextConfig;
